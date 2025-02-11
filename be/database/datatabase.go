5508242e24bffe667a34113171dd3a6cd6950c0c
IksanN03 <iksannursalim12345@gmail.com>
2025-02-11 11:10:13 +0700

    msg:update db

--- be/database/datatabase.go
+++ be/database/datatabase.go
@@ -4,6 +4,8 @@ import (
 	"be-test/models"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -15,6 +17,30 @@ var DB *gorm.DB
 // Init initializes the database connection
 func Init() {
 	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := "postgres://" + os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + "/" + os.Getenv("DB_NAME") + "?sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+
+	DB = db
+
+	// Auto migrate the models
+	DB.AutoMigrate(&models.Inventory{}, &models.User{}, &models.Recipe{})
+}
+
+// Init initializes the database connection
+func InitTest() {
+	// Get the current file's directory
+	_, b, _, _ := runtime.Caller(0)
+	rootPath := filepath.Join(filepath.Dir(b), "..")
+
+	// Load .env from root directory
+	err := godotenv.Load(filepath.Join(rootPath, ".env"))
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
